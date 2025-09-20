e552b6606a6ab8cd21d829ea98d8cebfe616582a
yresolutions <yreyesolutions@gmail.com>
2025-09-19 22:58:52 -0400

    Eliminando de la estructura de proyectos la carpete "src"

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import app from './app.js'
 import config from './config/index.js'
 
+// Conecto la Base de Datos
+
+// Inicio el servidor solo si la Base de Datos se 
 app.listen(config.PORT, () => {
   console.log(`Server is running on http://localhost:${config.PORT}`)
 })
